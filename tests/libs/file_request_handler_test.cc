e606b5cbfba7d6d3e83b64d41b3975f6888c8618
md842 <deng.m.842@gmail.com>
2024-07-06 11:01:05 -0700

    Added file request handler unit tests
    WHAT: Added unit tests to bring coverage for src/file_request_handler.cc to 100%.
    WHY: Testing is good!
    TESTING: Unit and integration tests pass.

--- tests/libs/file_request_handler_test.cc
+++ tests/libs/file_request_handler_test.cc
@@ -1,8 +1,12 @@
 #include <boost/filesystem.hpp> // boost::filesystem::current_path()
+#include <boost/filesystem/fstream.hpp>
 #include <memory> // std::unique_ptr
 
 #include "file_request_handler.h"
 #include "gtest/gtest.h"
+
+std::string get_content_length(Response res); // Helper
+std::string get_content_type(Response res); // Helper
 
 class FileRequestHandlerTest : public ::testing::Test {
 protected:
@@ -24,12 +28,60 @@ protected:
   }
 };
 
-TEST_F(FileRequestHandlerTest, ServeHTML){ // Uses test fixture
+TEST_F(FileRequestHandlerTest, ConnectionClose){ // Uses test fixture
+  std::string full_path = root_dir + "/tests/inputs/small.html";
   std::unique_ptr<FileRequestHandler> file_request_handler =
-    std::make_unique<FileRequestHandler>(root_dir + "/tests/inputs/small.html");
+    std::make_unique<FileRequestHandler>(full_path);
+
+  req.set("Connection", "close");
+
   Response* res = file_request_handler->handle_request(req);
   EXPECT_EQ(res->version(), 11);
   EXPECT_EQ(res->result_int(), 200);
+  EXPECT_FALSE(res->keep_alive());
+  EXPECT_EQ(get_content_type(*res), "text/html");
+
+  free(res);
+  file_request_handler.reset();
+}
+
+TEST_F(FileRequestHandlerTest, Create){
+  std::string full_path = root_dir + "/tests/inputs/small.html";
+  std::unique_ptr<FileRequestHandlerFactory> factory =
+    std::make_unique<FileRequestHandlerFactory>();
+  RequestHandler* file_request_handler = factory->create(full_path);
+
+  Response* res = file_request_handler->handle_request(req);
+  EXPECT_EQ(res->result_int(), 200);
+
+  free(res);
+  free(file_request_handler);
+  factory.reset();
+}
+
+TEST_F(FileRequestHandlerTest, ServeDir){ // Uses test fixture
+  std::string full_path = root_dir + "/tests/inputs";
+  std::unique_ptr<FileRequestHandler> file_request_handler =
+    std::make_unique<FileRequestHandler>(full_path);
+
+  Response* res = file_request_handler->handle_request(req);
+  EXPECT_EQ(res->version(), 11);
+  EXPECT_EQ(res->result_int(), 404);
+  EXPECT_TRUE(res->keep_alive());
+  EXPECT_EQ(get_content_type(*res), "text/html");
+
+  free(res);
+  file_request_handler.reset();
+}
+
+TEST_F(FileRequestHandlerTest, ServeHTML){ // Uses test fixture
+  std::unique_ptr<FileRequestHandler> file_request_handler =
+    std::make_unique<FileRequestHandler>(root_dir +
+      "/tests/inputs/small.html");
+  Response* res = file_request_handler->handle_request(req);
+  EXPECT_EQ(res->version(), 11);
+  EXPECT_EQ(res->result_int(), 200);
+  EXPECT_TRUE(res->keep_alive());
   EXPECT_EQ(res->body(), "<!DOCTYPE html>\n"
                          "<html lang=\"en\">\n"
                          "  <head>\n"
@@ -40,6 +92,91 @@ TEST_F(FileRequestHandlerTest, ServeHTML){ // Uses test fixture
                          "  </body>\n"
                          "</html>"
   );
+  EXPECT_EQ(get_content_type(*res), "text/html");
+
   free(res);
   file_request_handler.reset();
+}
+
+TEST_F(FileRequestHandlerTest, ServeInaccessible){ // Uses test fixture
+  std::string full_path = root_dir + "/tests/inputs/no_permission.html";
+  std::unique_ptr<FileRequestHandler> file_request_handler =
+    std::make_unique<FileRequestHandler>(full_path);
+
+  // Make the file inaccessible by changing its permissions
+  chmod(full_path.c_str(), 0000);
+  
+  Response* res = file_request_handler->handle_request(req);
+  EXPECT_EQ(res->version(), 11);
+  EXPECT_EQ(res->result_int(), 500);
+  EXPECT_TRUE(res->keep_alive());
+  EXPECT_EQ(get_content_type(*res), "text/plain");
+
+  chmod(full_path.c_str(), 0755); // Make the file accessible again
+  free(res);
+  file_request_handler.reset();
+}
+
+TEST_F(FileRequestHandlerTest, ServeLarge){ // Uses test fixture
+  std::unique_ptr<FileRequestHandler> file_request_handler =
+    std::make_unique<FileRequestHandler>(root_dir +
+      "/tests/inputs/large.html");
+  Response* res = file_request_handler->handle_request(req);
+  EXPECT_EQ(res->version(), 11);
+  EXPECT_EQ(res->result_int(), 200);
+  EXPECT_TRUE(res->keep_alive());
+  EXPECT_EQ(get_content_length(*res), "1068184");
+  EXPECT_EQ(get_content_type(*res), "text/html");
+
+  free(res);
+  file_request_handler.reset();
+}
+
+TEST_F(FileRequestHandlerTest, ServeNonexistent){ // Uses test fixture
+  std::string full_path = root_dir + "/tests/inputs/thisdoesnotexist.html";
+  std::unique_ptr<FileRequestHandler> file_request_handler =
+    std::make_unique<FileRequestHandler>(full_path);
+
+  Response* res = file_request_handler->handle_request(req);
+  EXPECT_EQ(res->version(), 11);
+  EXPECT_EQ(res->result_int(), 404);
+  EXPECT_TRUE(res->keep_alive());
+  EXPECT_EQ(get_content_type(*res), "text/html");
+
+  free(res);
+  file_request_handler.reset();
+}
+
+TEST_F(FileRequestHandlerTest, ServeOctetStream){ // Uses test fixture
+  std::string full_path = root_dir + "/tests/inputs/octet_stream";
+  std::unique_ptr<FileRequestHandler> file_request_handler =
+    std::make_unique<FileRequestHandler>(full_path);
+
+  Response* res = file_request_handler->handle_request(req);
+  EXPECT_EQ(res->version(), 11);
+  EXPECT_EQ(res->result_int(), 200);
+  EXPECT_TRUE(res->keep_alive());
+  EXPECT_EQ(res->body(), "This file has no extension!");
+  EXPECT_EQ(get_content_type(*res), "application/octet-stream");
+
+  free(res);
+  file_request_handler.reset();
+}
+
+// Helper function to extract Content-Type header
+std::string get_content_length(Response res){
+  for (auto& header : res.base()){
+    if (header.name_string() == "Content-Length")
+      return header.value();
+  }
+  return "";
+}
+
+// Helper function to extract Content-Type header
+std::string get_content_type(Response res){
+  for (auto& header : res.base()){
+    if (header.name_string() == "Content-Type")
+      return header.value();
+  }
+  return "";
 }
