efe3d7b46f59d30146b7a32bdc0e9993a97cb0bb
md842 <deng.m.842@gmail.com>
2025-01-02 10:44:51 -0800

    Migrated projects database
    WHAT: Migrated projects database to a database in the same Google Cloud project.
    WHY: I made a Google Cloud project for the Firestore database long before I made one for the webserver hosting and forgot to put them in the same Google Cloud project. Fixing that now so everything is in one place.
    TESTING: Website functionality unchanged. Unit and integration tests pass.

--- frontend/src/components/firebaseConfig.ts
+++ frontend/src/components/firebaseConfig.ts
@@ -1,25 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
 import { getFirestore } from "firebase/firestore";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD-r7iSyxMrE6NFel_BVzq1GiX6WvScqB8",
-  authDomain: "max-deng-personal-website.firebaseapp.com",
-  databaseURL: "https://max-deng-personal-website-default-rtdb.firebaseio.com",
-  projectId: "max-deng-personal-website",
-  storageBucket: "max-deng-personal-website.appspot.com",
-  messagingSenderId: "589193528004",
-  appId: "1:589193528004:web:ef6b32cda6626dfdbebf91"
+  apiKey: "AIzaSyAipHxxG4m4EheYoz2ciL11aXw1mPy0-xs",
+  authDomain: "my-web-server-445101.firebaseapp.com",
+  projectId: "my-web-server-445101",
+  storageBucket: "my-web-server-445101.firebasestorage.app",
+  messagingSenderId: "464336319665",
+  appId: "1:464336319665:web:1ef9dd8e8699930a9b9e90"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 const db = getFirestore(app);
 
 export default db;
