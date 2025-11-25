6f4420022d621be6f41a1d49a20064bceb2f4ece
Max Deng <deng.m.842@gmail.com>
2025-11-25 13:37:24 -0800

    Redesigned integration test for separate frontend
    WHAT: Integration test now allows the user to specify the frontend production build directory and the file to use for the static file serving test.
    WHY: Preparing to separate the frontend directory from this repository.
    TESTING: Integration test passes with the frontend directory separate from the webserver directory.

--- tests/integration/integration_tests.sh
+++ tests/integration/integration_tests.sh
@@ -1,16 +1,23 @@
 #! /bin/bash
 
+# Set file path for frontend production build directory (relative to webserver root)
+FRONTEND_DIR=../personal-website/build/
+# Set file path for the server config file to use for the integration test (relative to webserver root)
+CONFIG_FILE=configs/local_config.conf
+# File name to be used for the static file serving integration test (relative to frontend public directory)
+STATIC_TEST_FILE=test.txt
+
+# Set file paths for integration test output and result files (relative to webserver root)
+OUTPUT_FILE=tests/integration/last_test_output.txt
+RESULT_FILE=tests/integration/last_test_result.txt
+
 # Resolve server binary path to be used for integration testing
 if [ $# = 0 ] # No arguments supplied
 then
-  SERVER_BINARY=./build/bin/server # Use default server binary path
+  SERVER_BINARY=build/bin/server # Use default server binary path
 else # Argument(s) supplied
   SERVER_BINARY=$1 # Set server binary path to supplied argument
 fi
-
-# Set file paths for test output and result files (relative to project root)
-OUTPUT=tests/integration/last_test_output.txt
-RESULT=tests/integration/last_test_result.txt
 
 # Usage:
 # $1: Path to file containing expected output
@@ -24,20 +31,20 @@ integration_test (){
   then
     $2 $3 # Run curl, background process not necessary as curl terminates on its own
   else
-    $2 $3 < $4 > $OUTPUT & # Run netcat command as background process, redirecting stdin to $4, stdout to output
+    $2 $3 < $4 > $OUTPUT_FILE & # Run netcat command as background process, redirecting stdin to $4, stdout to output
   fi
 
   sleep 0.1 # Small delay for server processing/output to become available
-  diff $OUTPUT $1 # Compare actual output to expected output
+  diff $OUTPUT_FILE $1 # Compare actual output to expected output
   DIFF_CODE=$? # diff uses exit codes to indicate file equality
 
   if [ $DIFF_CODE != 0 ] # diff returned nonzero; files are not equal
   then
     if [ $2 = "curl" ] # If command is curl
       then
-        printf "Test $NUM FAIL: $2 $3\n" >> $RESULT # Print info about curl
+        printf "Test $NUM FAIL: $2 $3\n" >> $RESULT_FILE # Print info about curl
       else
-        printf "Test $NUM FAIL: $2 $3 < $4\n" >> $RESULT # Print info about netcat
+        printf "Test $NUM FAIL: $2 $3 < $4\n" >> $RESULT_FILE # Print info about netcat
     fi
     kill $WEBSERVER_PID # Shut down web server on test fail
     exit $DIFF_CODE # Exit early on test fail
@@ -45,27 +52,27 @@ integration_test (){
 
   if [ $2 = "curl" ] # If command is curl
   then
-    printf "Test $NUM PASS: $2 $3\n" >> $RESULT # Print info about curl
+    printf "Test $NUM PASS: $2 $3\n" >> $RESULT_FILE # Print info about curl
   else
-    printf "Test $NUM PASS: $2 $3 < $4\n" >> $RESULT # Print info about netcat
+    printf "Test $NUM PASS: $2 $3 < $4\n" >> $RESULT_FILE # Print info about netcat
   fi
 }
 
 cd ../../ # Move to project root directory
-rm $RESULT # Remove previous test results
-touch $RESULT # Create new test results file
+rm $RESULT_FILE # Remove previous test results
+touch $RESULT_FILE # Create new test results file
 NUM=0 # Initialize test number counter
-$SERVER_BINARY configs/local_config.conf & # Ending with & runs command as background process
+$SERVER_BINARY $CONFIG_FILE & # Ending with & runs command as background process
 WEBSERVER_PID=$! # Save PID of web server to shut it down after tests are done
 sleep 0.1 # Give the server time to start up
 
-# Function call  $1: Expected output file                       $2: Command   $3: Options                                                   $4 Netcat input file (omit for curl)
-integration_test "frontend/build/index.html"                    "curl"        "-k -o $OUTPUT -s https://localhost:8080/"
-integration_test "frontend/public/test.txt"                     "curl"        "-k -o $OUTPUT -s https://localhost:8080/test.txt"
-integration_test "tests/nc/outputs/leave_dir.txt"               "nc"          "localhost 8081"                                              "tests/nc/inputs/leave_dir.txt"
-integration_test "tests/nc/outputs/invalid_method.txt"          "nc"          "localhost 8081"                                              "tests/nc/inputs/invalid_method.txt"
-integration_test "tests/nc/outputs/redirect_http_to_https.txt"  "nc"          "localhost 8079"                                              "tests/nc/inputs/redirect_test.txt"
-integration_test "tests/nc/outputs/redirect_same_scheme.txt"    "nc"          "localhost 8082"                                              "tests/nc/inputs/redirect_test.txt"
-# Function call  $1: Expected output file                       $2: Command   $3: Options                                                   $4 Netcat input file (omit for curl)
+# Function call  $1: Expected output file                       $2: Command   $3: Options                                                         $4 Netcat input file (omit for curl)
+integration_test "${FRONTEND_DIR}index.html"                    "curl"        "-k -o $OUTPUT_FILE -s https://localhost:8080/"
+integration_test "${FRONTEND_DIR}${STATIC_TEST_FILE}"           "curl"        "-k -o $OUTPUT_FILE -s https://localhost:8080/${STATIC_TEST_FILE}"
+integration_test "tests/nc/outputs/leave_dir.txt"               "nc"          "localhost 8081"                                                    "tests/nc/inputs/leave_dir.txt"
+integration_test "tests/nc/outputs/invalid_method.txt"          "nc"          "localhost 8081"                                                    "tests/nc/inputs/invalid_method.txt"
+integration_test "tests/nc/outputs/redirect_http_to_https.txt"  "nc"          "localhost 8079"                                                    "tests/nc/inputs/redirect_test.txt"
+integration_test "tests/nc/outputs/redirect_same_scheme.txt"    "nc"          "localhost 8082"                                                    "tests/nc/inputs/redirect_test.txt"
+# Function call  $1: Expected output file                       $2: Command   $3: Options                                                         $4 Netcat input file (omit for curl)
 
 kill $WEBSERVER_PID # Shut down web server after all tests have finished. Also ends any netcat background processes that are still alive.
