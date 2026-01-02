2c3e1c9cbaffb1f0f9b52ba832ed3bef6ac75a90
Max Deng <deng.m.842@gmail.com>
2026-01-02 14:56:22 -0800

    Build script takes optional build type argument
    WHAT: `build.sh` now takes CMake build type as an optional argument.
    WHY: This offers a more convenient way to test the Release build, or simply use the build script without the additional dependencies required by the default Debug build type.
    TESTING: `./build.sh`, `./build.sh Debug`, `./build.sh Coverage`, and `./build.sh Release` all result in the expected outcomes. Additionally, `./build.sh Debug` succeeds without dependency `gcovr`.  `./build.sh Release` succeeds without dependencies `curl`, `gcovr`, `gtest`, and `netcat`.

--- build.sh
+++ build.sh
@@ -2,5 +2,10 @@
 # A script that automates build commands for convenience while testing.
 mkdir build
 cd build
-cmake ..
+if [ $# -eq 1 ] # Build type provided as argument
+then
+    cmake -DCMAKE_BUILD_TYPE=$1 .. # Build with specified build type
+else # 0 arguments provided
+    cmake .. # Defaults to Debug build type
+fi
 make
